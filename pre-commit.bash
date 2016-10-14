7e3fabd8a7aef412d8dd7180af81f487da9efd61
Tim Heilman <tim@dv8.org>
2016-10-14 16:21:00 -0700

    Pre-commit hook for common cloze deletion error.

---
+++ pre-commit.bash
@@ -0,0 +1,9 @@
+# link or include this in .git/hooks/pre-commit to prevent accidental inclusion of a or an prior to
+# cloze deletions that give away what has been deleted
+
+ARTICLE_REJECTION="commit rejected: replace 'a' with 'a/an' for indefinite articles introducing cloze deletions.";
+A_CLOZE_PATTERN='^+.*[aA] {{';
+AN_CLOZE_PATTERN='^+.*[aA][nN] {{';
+for cloze_pattern in "$A_CLOZE_PATTERN" "$AN_CLOZE_PATTERN"; do
+    git diff --cached | grep "$cloze_pattern" && echo $ARTICLE_REJECTION && exit 1;
+done
