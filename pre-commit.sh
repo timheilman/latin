8a21584b81f4b38e2de9aed6e1fe3b6a689e4ad3
Tim Heilman <tim@dv8.org>
2016-12-30 11:18:13 -0800

    Trying to fix pre commit hook.

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,14 +3,16 @@
 # cloze deletions that give away what has been deleted
 
 ARTICLE_REJECTION="commit rejected: replace 'a' with 'a/an' for indefinite articles introducing cloze deletions.";
-A_CLOZE_PATTERN='[aA] {{';
-AN_CLOZE_PATTERN='[^/][aA][nN] {{';
+A_CLOZE_PATTERN='^+.*[aA] {{';
+AN_CLOZE_PATTERN='^+.*[^/][aA][nN] {{';
 for cloze_pattern in "$A_CLOZE_PATTERN" "$AN_CLOZE_PATTERN"; do
     git diff --cached | grep "$cloze_pattern" && echo $ARTICLE_REJECTION && exit 1;
 done
 
 HTML_REJECTION="commit rejected: angle brackets or ampersands found.  Anki blames the user to handle edge cases with regard to import and subsequent editing of text that is not valid HTML.  Simply avoid ampersands and angle brackets (among others) to avoid confusing behavior (like failed duplicate detection at import despite subsequent duplicate detection in the browser, including identically rendered text into the text field)."
-HTML_PATTERN='[<>&]'
+HTML_PATTERN='^+.*[<>&]'
 git diff --cached | grep "$HTML_PATTERN" && echo $HTML_REJECTION && exit 1;
+
+echo "Pre-commit succeeded."
 
 exit 0
